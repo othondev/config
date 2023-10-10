51ecc416df84561be4e064788b63dc16117845ba
Othon Ferreira <othondev@gmail.com>
2023-10-10 21:39:02 +1000

    More shortcut

--- nvim/lua/othondev/plugins/fugitive.lua
+++ nvim/lua/othondev/plugins/fugitive.lua
@@ -1,3 +1,7 @@
 return {
-  'tpope/vim-fugitive'
+  'tpope/vim-fugitive',
+  config = function()
+    vim.api.nvim_set_keymap('n', '<leader>gg', ':Git<CR>', { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<leader>gb', ':Git blame<CR>', { noremap = true, silent = true })
+  end
 }
--- nvim/lua/othondev/plugins/telescope.lua
+++ nvim/lua/othondev/plugins/telescope.lua
@@ -2,11 +2,10 @@ return {
   'nvim-telescope/telescope.nvim', tag = '0.1.3',
   requires = { {'nvim-lua/plenary.nvim'} },
   config = function()
-
-    local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-    vim.keymap.set('n', '<leader><BS>', builtin.oldfiles(), {})
+    vim.api.nvim_set_keymap('n', '<leader>ff', [[<cmd>lua require('telescope.builtin').find_files()<CR>]], { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<leader>fg', [[<cmd>lua require('telescope.builtin').live_grep()<CR>]], { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<leader>fb', [[<cmd>lua require('telescope.builtin').buffers()<CR>]], { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<leader><BS>', [[<cmd>lua require('telescope.builtin').oldfiles()<CR>]], { noremap = true, silent = true })
+    vim.api.nvim_set_keymap('n', '<leader><Space>', [[<cmd>lua require('telescope.builtin').commands()<CR>]], { noremap = true, silent = true })
   end
 }
