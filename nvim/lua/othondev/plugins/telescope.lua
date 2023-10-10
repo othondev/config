08768de89ed8abce0eff225c57d5867a00c31941
Othon Ferreira <othondev@gmail.com>
2023-10-10 21:03:55 +1000

    Changed telescope

--- nvim/lua/othondev/plugins/telescope.lua
+++ nvim/lua/othondev/plugins/telescope.lua
@@ -3,7 +3,10 @@ return {
   requires = { {'nvim-lua/plenary.nvim'} },
   config = function()
 
-    vim.api.nvim_set_keymap('n', '<leader>ff', [[<cmd>lua require('telescope.builtin').find_files()<CR>]], { noremap = true, silent = true })
-
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+    vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+    vim.keymap.set('n', '<leader><BS>', builtin.oldfiles(), {})
   end
 }
