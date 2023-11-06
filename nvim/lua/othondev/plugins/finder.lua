724e4fa5f61fca3bb6d85dfed063b87d27d9a1bc
Othon Ferreira <othondev@gmail.com>
2023-11-06 23:07:51 +1000

    feat: Add git and finder feature

---
+++ nvim/lua/othondev/plugins/finder.lua
@@ -0,0 +1,48 @@
+return {
+  'nvim-telescope/telescope.nvim',
+  tag = '0.1.0',
+  requires = {
+    -- ???
+    {'nvim-lua/plenary.nvim'},
+    -- ???
+    {'folke/trouble.nvim'},
+  },
+  config = function()
+    require("trouble").setup {
+      icons = false,
+    }
+
+    -- Telescope maps
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+    vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+    vim.keymap.set('n', '<leader>ps', function()
+      builtin.grep_string({ search = vim.fn.input("Grep > ") })
+    end)
+    vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+
+    -- Trouble maps
+    vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
+    vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end)
+    vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
+    vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
+    vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
+    vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
+
+    -- Telescope X Trouble
+    local actions = require("telescope.actions")
+    local trouble = require("trouble.providers.telescope")
+
+    local telescope = require("telescope")
+
+    telescope.setup {
+      defaults = {
+        mappings = {
+          i = { ["<c-t>"] = trouble.open_with_trouble },
+          n = { ["<c-t>"] = trouble.open_with_trouble },
+        },
+      },
+    }
+
+  end
+}
---
+++ nvim/lua/othondev/plugins/git_integration.lua
@@ -0,0 +1,43 @@
+return {
+  'tpope/vim-fugitive',
+  requires = {
+    {'lewis6991/gitsigns.nvim'}
+  },
+  config = function()
+    vim.keymap.set("n", "<leader>gg", vim.cmd.Git)
+
+    require('gitsigns').setup {
+      signs = {
+        add          = { text = '+' },
+        change       = { text = '~' },
+        delete       = { text = '_' },
+        topdelete    = { text = '‾' },
+        changedelete = { text = '~' },
+        untracked    = { text = '┆' },
+      },
+
+      on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map('n', ']c', function()
+          if vim.wo.diff then return ']c' end
+          vim.schedule(function() gs.next_hunk() end)
+          return '<Ignore>'
+        end, {expr=true})
+
+        map('n', '[c', function()
+          if vim.wo.diff then return '[c' end
+          vim.schedule(function() gs.prev_hunk() end)
+          return '<Ignore>'
+        end, {expr=true})
+      end
+    }
+  end
+}
