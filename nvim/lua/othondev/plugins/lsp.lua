549050474455ee4aadd06e3af29f69842376ada5
Othon Ferreira <othondev@gmail.com>
2023-11-09 00:06:19 +1000

    Fix lsp shortcut

--- nvim/lua/othondev/plugins/lsp.lua
+++ nvim/lua/othondev/plugins/lsp.lua
@@ -53,7 +53,7 @@ return {
       ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
       ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
       ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-      ["<C-Space>"] = cmp.mapping.complete(),
+      ["<C-l>"] = cmp.mapping.complete(),
     })
 
     cmp_mappings['<Tab>'] = nil
@@ -78,13 +78,13 @@ return {
 
       vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
       vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-      vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+      vim.keymap.set("n", "<leader>s", function() vim.lsp.buf.workspace_symbol() end, opts)
       vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
       vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
       vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
       vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
       vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-      vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+      vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
       vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
     end)
 
